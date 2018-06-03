6c0aca8d653909cb2f8ed656ec7ad857e130b9ad
pckurdu <39583402+pckurdu@users.noreply.github.com>
2018-06-03 17:54:23 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+const {app,BrowserWindow}=require('electron')
+const url=require('url')
+const path=require('path')
+
+let win;
+
+function createWindow(){
+  win=new BrowserWindow({
+    width:900,
+    height:700,
+    frame:false
+  })
+
+  win.loadURL(url.format({
+    pathname:path.join(__dirname,'index.html'),
+    protocol:'file:',
+    slashes:true
+  }))
+
+  win.openDevTools()
+}
+
+app.on('ready',createWindow)
+
+
